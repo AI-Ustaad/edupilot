822acf7f404154acd25e420403d907e10f347236
AI-Ustaad <sindhu.sgd143@gmail.com>
2026-04-03 14:27:06 +0500

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyCGAp4gOdmzXSkNlXR18RjKtZPwWEMfP64",
+  authDomain: "edupilot-d262f.firebaseapp.com",
+  projectId: "edupilot-d262f",
+  storageBucket: "edupilot-d262f.firebasestorage.app",
+  messagingSenderId: "1032307682046",
+  appId: "1:1032307682046:web:eb32d969227210c4f293af",
+  measurementId: "G-D62QEZQG4C"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
