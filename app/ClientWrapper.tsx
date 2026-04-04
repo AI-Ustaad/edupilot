a6119a086d9a6757092e15e71380ee7b75dea840
AI-Ustaad <sindhu.sgd143@gmail.com>
2026-04-04 10:39:40 +0500

    Update ClientWrapper.tsx

--- app/ClientWrapper.tsx
+++ app/ClientWrapper.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
-import { auth } from "./lib/firebase";
+import { auth } from "../lib/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import SidebarLayout from "./components/SidebarLayout";
 
