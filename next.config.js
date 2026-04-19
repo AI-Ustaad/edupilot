eae6ce42ac12b056d31e362d83e492f0b14aeac1
AI-Ustaad <sindhu.sgd143@gmail.com>
2026-04-19 17:24:17 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
