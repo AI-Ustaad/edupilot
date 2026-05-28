9859cb312d9ddc78baa89b69e2812fa5abbf3aa3
AI-Ustaad <sindhu.sgd143@gmail.com>
2026-05-28 11:14:35 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withNextIntl = require("next-intl/plugin")();
+
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig = {
@@ -23,4 +24,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = withNextIntl(withPWA(nextConfig));
