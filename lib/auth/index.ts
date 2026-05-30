128e5b05addd3a8affe64cea21d3e160f9b71438
AI-Ustaad <sindhu.sgd143@gmail.com>
2026-05-30 18:00:41 +0500

    Update index.ts

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,5 +1,2 @@
-// lib/auth/index.ts
-export * from "./auth-client";
-export * from "./auth-server";
-export * from "./permissions";
-export * from "./rbac";
+export * from './permissions';
+export * from './rbac';
