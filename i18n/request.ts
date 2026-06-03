a616c077667ff6e26daf8440cbeefa84ca9e4c41
AI-Ustaad <sindhu.sgd143@gmail.com>
2026-06-03 08:52:10 +0500

    Update request.ts

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,10 +1,6 @@
 import { getRequestConfig } from "next-intl/server";
 
-export default getRequestConfig(async ({ locale }) => {
-  const currentLocale = locale || "en";
-
-  return {
-    locale: currentLocale,
-    messages: (await import(`../messages/${currentLocale}.json`)).default,
-  };
-});
+export default getRequestConfig(async () => ({
+  locale: "en",
+  messages: (await import("../messages/en.json")).default,
+}));
