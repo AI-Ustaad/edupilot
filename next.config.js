f63966d5dbbef7b9d3bb781031172957cb2bcee0
AI-Ustaad <sindhu.sgd143@gmail.com>
2026-06-10 18:45:23 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withNextIntl = require("next-intl/plugin")("./i18n/request.ts");
 
 const nextConfig = {
-  // ✅ FIX: Mark exceljs as server-only package (prevents webpack bundling errors)
-  serverExternalPackages: ['exceljs'],
+  // ✅ FIX: Next.js 14.2.3 کے لیے experimental option
+  experimental: {
+    serverComponentsExternalPackages: ['exceljs'],
+  },
 
   async rewrites() {
     return [
@@ -19,37 +21,14 @@ module.exports = withNextIntl(nextConfig);
 const { withSentryConfig } = require("@sentry/nextjs");
 
 module.exports = withSentryConfig(module.exports, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
   org: "ai-ustaad",
   project: "javascript-nextjs",
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
   webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.com/product/crons/
-    // https://vercel.com/docs/cron-jobs
     automaticVercelMonitors: true,
-
-    // Tree-shaking options for reducing bundle size
     treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
       removeDebugLogging: true,
     },
   },
