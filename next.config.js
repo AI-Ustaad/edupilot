2bbc6f95f6b525ad3159bbdca56ecea3e25b6418
AI-Ustaad <sindhu.sgd143@gmail.com>
2026-06-13 17:56:04 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,41 +1,15 @@
-const withNextIntl = require("next-intl/plugin")("./i18n/request.ts");
+// Injected content via Sentry wizard below
+const { withSentryConfig } = require("@sentry/nextjs");
 
-const nextConfig = {
-  async rewrites() {
-    return [
-      { source: "/api/:path*", destination: "/api/v1/:path*" },
-    ];
+module.exports = withSentryConfig(configWithIntl, {
+  org: "ai-ustaad",
+  project: "javascript-nextjs",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  webpack: {
+    automaticVercelMonitors: true,
+    treeshake: {
+      removeDebugLogging: true,
+    },
   },
-  
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      if (Array.isArray(config.externals)) {
-        config.externals.push(function({ request }, callback) {
-          if (request === 'xlsx') {
-            return callback(null, 'commonjs xlsx');
-          }
-          callback();
-        });
-      }
-    }
-    return config;
-  },
-};
-
-module.exports = withNextIntl(nextConfig);
-
-// Sentry temporarily disabled for debugging
-// const { withSentryConfig } = require("@sentry/nextjs");
-// module.exports = withSentryConfig(module.exports, {
-//   org: "ai-ustaad",
-//   project: "javascript-nextjs",
-//   silent: !process.env.CI,
-//   widenClientFileUpload: true,
-//   webpack: {
-//     automaticVercelMonitors: true,
-//     treeshake: {
-//       removeDebugLogging: true,
-//     },
-//   },
-// });
+});
