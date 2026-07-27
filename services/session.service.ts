7723280d14db5c1c3d6de5e6e4d749ff248517c2
AI-Ustaad <sindhu.sgd143@gmail.com>
2026-07-27 17:16:40 +0500

    Update session.service.ts

--- services/session.service.ts
+++ services/session.service.ts
@@ -6,6 +6,9 @@ export class SessionService implements ISessionService {
   constructor(private authRepo = new AuthRepository()) {}
 
   async createCookie(idToken: string): Promise<string> {
-    return await this.authRepo.createSessionCookie(idToken, { expiresIn: SESSION_EXPIRES_IN_MS });
+    return this.authRepo.createSessionCookie(
+      idToken,
+      SESSION_EXPIRES_IN_MS
+    );
   }
 }
